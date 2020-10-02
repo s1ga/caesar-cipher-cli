c83b0c2679984c0b02c407b5b835d82c0f500719
Sergey Harlanov <s.v.harlanov@gmail.com>
2020-10-02 18:30:08 +0300

    feat: add options for CLI

--- caesar-cipher/my_caesar_cli.js
+++ caesar-cipher/my_caesar_cli.js
@@ -2,4 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const commander = require('commander')
 const {} = require("stream")
+
+//our caesar cipher function
 const caesarCipher = require('./caesarCipher')
+
+
+//creating flags for app
+program
+    .storeOptionsAsProperties(true)
+    .passCommandToAction(true)
+program.version('0.0.1')
+program
+    .requiredOption('-a, --action <name>', 'Action: encode or decode')
+    .requiredOption('-s, --shift <number>', 'Shift of cipher')
+    .option('-i, --input <path>', 'File from which cipher will be read')
+    .option('-o, --output <path>', 'File where cipher will be written')
+    .parse(process.argv)
